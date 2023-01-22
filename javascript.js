547153ad33fd47294ce9f397bd094fcd5dc6a85e
Elman Islam <eislam@uoguelph.ca>
2023-01-22 14:27:54 -0500

    Add input slider for grid dimensions

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
-function setBoxes() {
+//create a number of divs in the grid, each of class 'box'
+function setBoxes( numSquaresPerSide = 16) {
   const container = document.querySelector('.grid');
-  for (let i = 0; i < 16; i++)
+  for (let i = 0; i < numSquaresPerSide; i++)
   {
-    for (let j = 0; j < 16; j++)
+    for (let j = 0; j < numSquaresPerSide; j++)
     {
       const box = document.createElement('div');
       box.className = 'box';
@@ -11,5 +12,32 @@ function setBoxes() {
   }
 
 }
+
+function clearBoxes() {
+  const myNode = document.querySelector('.grid');
+  while (myNode.lastElementChild) {
+    myNode.removeChild(myNode.lastElementChild);
+  }
+}
+
+
+function readSlider(e) {
+  // read the value from the slider:
+  clearBoxes();
+  let grid = document.querySelector('.grid');
+
+  grid.removeAttribute('style');
+  grid.setAttribute('style', 
+  `grid-template-rows: repeat(${e.target.value}, minmax(10px, 1fr));
+   grid-template-columns: repeat(${e.target.value}, minmax(10px, 1fr));`);
+
+  setBoxes(e.target.value);
+}
+
+let rangeInput = document.getElementById("boxInput");
+
+rangeInput.addEventListener('mouseup', readSlider);
+
+
 
 setBoxes();
